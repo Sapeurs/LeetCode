0f1e646a2212878ac055bce2bf25e5ad113f69dd
Sapeurs <chuan_hui_w@163.com>
2021-08-02 18:25:36 +0800

    2

--- src/Dp/problem39/test.java
+++ src/Dp/problem39/test.java
@@ -5,7 +5,14 @@ import java.util.Arrays;
 /**
  * @author: Sapeurs
  * @date: 2021/7/31 20:10
- * @description:
+ * @description: 300. 最长递增子序列
+ *
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素
+ * 而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ *
+ *
  */
 public class test {
     public static void main(String[] args) {
@@ -17,17 +24,15 @@ public class test {
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int len = nums.length;
-        int[] dp = new int[len];
+        if(nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
         int res = 0;
-        Arrays.fill(dp,1);
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i])
-                    //表示在i之前的j位置上的数字加上nums[i]后的最长上升子序列
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-            res = Math.max(dp[i], res);
+            res = Math.max(res, dp[i]);
         }
         return res;
     }
---
+++ src/Dp/problem40/test.java
@@ -0,0 +1,84 @@
+package Dp.problem40;
+
+import java.util.Arrays;
+
+/**
+ * @author: Sapeurs
+ * @date: 2021/8/1 20:59
+ * @description: 376. 摆动序列
+ *
+ * 如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。
+ * 第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素
+ * 的序列也视作摆动序列。
+ *
+ *子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素
+ * 保持其原始顺序。
+ *
+ * 给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度 。
+ *
+ */
+public class test {
+}
+
+
+
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+        int[] up = new int[len];
+        int[] down = new int[len];
+        up[0] = down[0] = 1;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[len - 1], down[len - 1]);
+    }
+
+    //空间优化
+    public int wiggleMaxLength1(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+        int up = 1;
+        int down = 1;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > nums[i - 1])
+                up = Math.max(up, down + 1);
+            else if (nums[i] < nums[i-1])
+                down = Math.max(down, up + 1);
+        }
+        return Math.max(up,down);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
